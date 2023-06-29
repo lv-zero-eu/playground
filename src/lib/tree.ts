cd95d8b69b99569219d9b4329cae27428091e41e
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-06-29 18:19:04 +0000

    Feat : Deep clone + Order less constructor

--- src/lib/tree.ts
+++ src/lib/tree.ts
@@ -1,13 +1,25 @@
+type NodeObject = {
+  name: string;
+  indent?: number;
+  parent?: Node | null | undefined;
+  expanded?: boolean;
+  children?: Node[];
+};
+
+
 export class Node {
   name: string;
-  children: Node[] = []
-  parent: Node | undefined = undefined;
+  children: Node[];
+  parent: Node | null;
   indent: number;
-  expanded = true
+  expanded: boolean;
 
-  constructor(name: string, indent?: number) {
+  constructor({ name, indent = 0, parent = null, expanded = true, children = [] }: NodeObject) {
     this.name = name;
-    this.indent = indent ? indent : 0
+    this.indent = indent
+    this.parent = parent
+    this.expanded = expanded
+    this.children = children
   }
 
   push(n: Node) {
@@ -21,7 +33,7 @@ export class Node {
     return this;
   }
 
-  toString() {
+  toString(): string {
     if (this.indent < 0) return this.children.map(n => n.toString()).join("")
     return (
       " ".repeat(this.indent) + // add indentation
@@ -43,32 +55,40 @@ export class Node {
     return this
   }
 
+  clone(): Node {
+    const clonedChildren = this.children.map(child => {
+      const clonedChild = child.clone();
+      clonedChild.parent = this; // Set parent reference for the cloned child
+      return clonedChild;
+    });
+    return new Node({ ...this, children: clonedChildren });
+  }
 }
 
 export const parseToTree = (s: string) => {
 
-  let root = new Node(".", -1);
+  let root = new Node({ name: ".", indent: -1 });
   let currentNode = root
 
   if (!s) return root
 
   s.split("\n").filter(s => s).forEach(line => {
-    const indentation = line.search(/\S/); // Find the index of the first non-whitespace character
-    if (indentation === -1) return;
-    const nodeName = line.trim();
-    const newNode = new Node(nodeName, indentation);
+    const indent = line.search(/\S/); // Find the index of the first non-whitespace character
+    if (indent === -1) return;
+    const name = line.trim();
+    const newNode = new Node({ name, indent });
 
 
-    if (indentation > currentNode.indent) {
+    if (indent > currentNode.indent) {
       currentNode = currentNode.push(newNode);
     }
-    else if (indentation === currentNode.indent) {
+    else if (indent === currentNode.indent) {
       currentNode = currentNode.parent.push(newNode)
     }
     else {
-      while (indentation <= currentNode.indent)
+      while (indent <= currentNode.indent)
         currentNode = currentNode.parent; // Move up the tree
-      const newNode = new Node(nodeName, indentation)
+      const newNode = new Node({ name, indent })
       currentNode = currentNode.push(newNode);
     }
 
