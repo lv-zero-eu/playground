cda330c7e28a344279c2a6fd11b330fe632b2e96
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-07-11 12:04:13 +0000

    Feat : circle progress example

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,29 @@
 <script lang="ts">
   import Tree from "./lib/Tree.svelte";
   import NotificationView from "./lib/notification/NotificationView.svelte";
+  import Circle from "./lib/progress/Circle.svelte";
+
+  let progress = 0;
+  let progressStatus = false;
+
+  setInterval(() => {
+    progress += !progressStatus ? 10 : -10;
+    progressStatus =
+      progress >= 100 || progress <= 0 ? !progressStatus : progressStatus;
+  }, 1000);
 </script>
 
 <main>
   <Tree />
   <NotificationView />
+  <Circle
+    {progress}
+    size={50}
+    strokeWidth={5}
+    color="green"
+    foregroundColor="red"
+    showProgress={true}
+  />
 </main>
 
 <style>
