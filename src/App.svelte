304d5265fec3ecd5175b66e958ddf07a1a9f5c5d
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-07-13 15:53:57 +0000

    More example

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,14 @@
   import { onDestroy, onMount } from "svelte";
 
   let progress = 0;
+  let antiProgress = 100;
   let progressStatus = false;
   let interval;
 
   onMount(() => {
     interval = setInterval(() => {
       progress += !progressStatus ? 10 : -10;
+      antiProgress += !progressStatus ? -10 : +10;
       progressStatus =
         progress >= 100 || progress <= 0 ? !progressStatus : progressStatus;
     }, 1000);
@@ -28,6 +30,33 @@
     {progress}
     size={150}
     strokeWidth={5}
+    color="red"
+    foregroundColor="green"
+    showProgress={true}
+    title="yes"
+  />
+  <Circle
+    progress={antiProgress}
+    size={150}
+    strokeWidth={5}
+    color="red"
+    foregroundColor="green"
+    showProgress={true}
+    title="yes"
+  />
+  <Circle
+    {progress}
+    size={200}
+    strokeWidth={8}
+    color="red"
+    foregroundColor="green"
+    showProgress={true}
+    title="yes"
+  />
+  <Circle
+    progress={antiProgress}
+    size={200}
+    strokeWidth={8}
     color="red"
     foregroundColor="green"
     showProgress={true}
