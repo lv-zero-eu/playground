a86d71a54d6c5302da6752188cb4255c566df7b4
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-07-18 12:49:59 +0000

    Fix better exemple

--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -1,17 +1,25 @@
 <!-- App.svelte -->
 
 <script>
+  import { onDestroy, onMount } from "svelte";
   import LinearChart from "./chart/Linear.svelte";
 
-  const chartData = [
+  let chartData = [
     { data: [50, 80, 120, 500, 60], color: "steelblue" },
     { data: [0, 100, 180, 130, -0], color: "orange" },
   ];
 
-  setInterval(() => {
-    chartData[0].data[1] += 1;
-    console.log(chartData[0]);
-  }, 1000);
+  let interval;
+
+  onMount(() => {
+    interval = setInterval(() => {
+      chartData[0].data[1] += 1;
+    }, 10);
+  });
+
+  onDestroy(() => {
+    clearInterval(interval);
+  });
 
   let fillArea = true; // Set to false if you don't want to fill the area
 </script>
@@ -21,5 +29,5 @@
   <label>
     <input type="checkbox" bind:checked={fillArea} /> Fill Area
   </label>
-  <LinearChart {chartData} width={400} height={200} {fillArea} />
+  <LinearChart {chartData} width={400} height={200} fillArea />
 </main>
