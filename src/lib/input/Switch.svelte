6826afa13b3ac8c31ac70e71329ce6353e530aa1
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-07-22 13:25:24 +0000

    Feat : switch on,off

---
+++ src/lib/input/Switch.svelte
@@ -0,0 +1,47 @@
+<script>
+  export let checked = false; // The initial state of the switch
+</script>
+
+<label>
+  <input type="checkbox" bind:checked />
+  <span class="switch">
+    <span class="circle" />
+  </span>
+</label>
+
+<style>
+  .switch {
+    display: inline-block;
+    width: 45px;
+    height: 25px;
+    background-color: #eeeeee;
+    border-radius: 15px;
+    position: relative;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .circle {
+    position: absolute;
+    top: 3px;
+    left: 3px;
+    height: calc(100% - 6px);
+    aspect-ratio: 1;
+    border-radius: 50%;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+    transition: transform 0.3s ease;
+  }
+
+  input[type="checkbox"] {
+    display: none;
+  }
+
+  input[type="checkbox"]:checked + .switch {
+    background-color: #4caf50; /* green when "on" */
+  }
+
+  input[type="checkbox"]:checked + .switch .circle {
+    transform: translateX(101%); /* move the circle to the right when "on" */
+  }
+</style>
